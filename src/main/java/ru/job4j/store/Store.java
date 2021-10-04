b13bad1574724eac68fe331b0b3342ca686eeef0
olliestyle <olliestyle1989@gmail.com>
2021-10-04 13:10:43 +0300

    2. CandidateSerlvet. Создание кандидата. [#2513]

--- src/main/java/ru/job4j/store/Store.java
+++ src/main/java/ru/job4j/store/Store.java
@@ -17,6 +17,8 @@ public class Store {
 
     private static AtomicInteger POST_ID = new AtomicInteger(4);
 
+    private static AtomicInteger CANDIDATE_ID = new AtomicInteger(4);
+
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "Job for Junior Java Developer"));
         posts.put(2, new Post(2, "Middle Java Job", "Job for Middle Java Developer"));
@@ -38,8 +40,13 @@ public class Store {
         return candidates.values();
     }
 
-    public void save(Post post) {
+    public void savePost(Post post) {
         post.setId(POST_ID.incrementAndGet());
         posts.put(post.getId(), post);
+    }
+
+    public void saveCandidate(Candidate candidate) {
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+        candidates.put(candidate.getId(), candidate);
     }
 }
