f7987faa4781adffd85e5ac99fbc1481dbad2526
olliestyle <olliestyle1989@gmail.com>
2021-10-04 14:02:51 +0300

    3. Редактирование вакансии. [#277566]

--- src/main/java/ru/job4j/servlet/CandidateServlet.java
+++ src/main/java/ru/job4j/servlet/CandidateServlet.java
@@ -13,7 +13,7 @@ public class CandidateServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().saveCandidate(new Candidate(0, req.getParameter("name")));
+        Store.instOf().saveCandidate(new Candidate(Integer.parseInt(req.getParameter("id")), req.getParameter("name")));
         resp.sendRedirect(req.getContextPath() + "/candidate/candidates.jsp");
     }
 }
--- src/main/java/ru/job4j/servlet/PostServlet.java
+++ src/main/java/ru/job4j/servlet/PostServlet.java
@@ -13,7 +13,7 @@ public class PostServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().savePost(new Post(0, req.getParameter("name"), req.getParameter("description")));
+        Store.instOf().savePost(new Post(Integer.parseInt(req.getParameter("id")), req.getParameter("name"), req.getParameter("description")));
         resp.sendRedirect(req.getContextPath() + "/post/posts.jsp");
     }
 }
